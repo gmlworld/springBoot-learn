fb92cef69012288fd9c6d8830d9968d9a16bf268
龚茂林 <gongmaolin@fybdp.com>
2020-07-07 17:37:10 +0800

    bean初始化过程备注

--- learn4/src/main/java/com/gongml/learn4/App.java
+++ learn4/src/main/java/com/gongml/learn4/App.java
@@ -7,7 +7,8 @@ import org.springframework.context.ConfigurableApplicationContext;
 @SpringBootApplication
 public class App {
     //beanDefinitionRegiestry -->> beanFactory -->>beanPostProcess
-    //beanDefinition -->> BeanFactory -->> BeanFactory InitializingBean  -->> BeanFactory initMethod ->>BeanPostProcess ->> BeanPostProcess PostConstruct ->>
+    // beanDefinition 为spring的bean的核心对象，BeanFactory包含beanDefinition,初始化bean的构造方法后存在钩子函数接口BeanPostProcess，可以调用bean的初始化与释放方法
+    //beanDefinition -->> BeanFactory  -->> BeanFactory  InitializingBean  -->> BeanFactory initMethod ->>BeanPostProcess ->> BeanPostProcess PostConstruct ->>
     //BeanPostProcess PreDestroy -->> BeanFactory destroyMethod -->> BeanFactory DisposableBean -->> distory
     //容器初始化 加载完bean文件，bean未初始化 加载BeanFactoryPostProcessor 可获取配置类信息 包括设定是否单例模式
     //beanPostProcessor bean初始化之后  先调用before 再调用bean的init方法，再调用after方法
--- learn4/src/main/java/com/gongml/learn4/MyJavaBean.java
+++ learn4/src/main/java/com/gongml/learn4/MyJavaBean.java
@@ -1,18 +1,27 @@
 package com.gongml.learn4;
 
 import lombok.Data;
+import org.springframework.beans.factory.InitializingBean;
 
 import javax.annotation.PostConstruct;
 
 @Data
-public class MyJavaBean {
+public class MyJavaBean implements InitializingBean {
     private String name ="gg";
     private int age;
     private String remark = "test";
 
-    @PostConstruct
-    public void  init(){
-        remark = "inited";
+    public MyJavaBean(){
+        System.out.println(1111111111);
     }
 
+//    @PostConstruct
+//    public void  init(){
+//        remark = "inited";
+//    }
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        remark = "inited";
+    }
 }
