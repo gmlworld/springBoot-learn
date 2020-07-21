e0951804f13549d8ecdc110c64ac0173ded0895b
龚茂林 <gongmaolin@fybdp.com>
2020-07-21 17:36:00 +0800

    bean初始化

--- learn1/src/main/java/com/gongml/learn1/AnnotationScan.java
+++ learn1/src/main/java/com/gongml/learn1/AnnotationScan.java
@@ -10,7 +10,7 @@ import org.springframework.context.annotation.FilterType;
  * @author: Gongml
  * @create: 2018-10-25 20:24
  **/
-@ComponentScan(basePackages = "com.lenrn1", excludeFilters = @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE,classes={UserController.class,UserService.class}))
+@ComponentScan(basePackages = "com.gongml.learn1", excludeFilters = @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE,classes={UserController.class,UserService.class}))
 @Configuration
 public class AnnotationScan {
 }
---
+++ learn4/src/main/java/com/gongml/learn4/MyInstantiationAwareBeanPostProcessor.java
@@ -0,0 +1,12 @@
+package com.gongml.learn4;
+
+import org.springframework.beans.factory.config.InstantiationAwareBeanPostProcessor;
+import org.springframework.stereotype.Component;
+
+/**
+ * @program: spring
+ * @description:
+ **/
+@Component
+public class MyInstantiationAwareBeanPostProcessor implements InstantiationAwareBeanPostProcessor {
+}
