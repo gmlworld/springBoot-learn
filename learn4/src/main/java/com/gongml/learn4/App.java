844b756d9366be249cdfbb7e8ebb5e830aa9508c
龚茂林 <gongmaolin@fybdp.com>
2020-07-23 10:48:54 +0800

    bean初始化过程备注

--- learn4/src/main/java/com/gongml/learn4/App.java
+++ learn4/src/main/java/com/gongml/learn4/App.java
@@ -8,7 +8,8 @@ import org.springframework.context.ConfigurableApplicationContext;
 public class App {
     //beanDefinitionRegiestry -->> beanFactory -->>beanPostProcess
     // beanDefinition 为spring的bean的核心对象，BeanFactory包含beanDefinition,初始化bean的构造方法后存在钩子函数接口BeanPostProcess，可以调用bean的初始化与释放方法
-    //beanDefinition -->> BeanFactory  -->> BeanFactory  InitializingBean  -->> BeanFactory initMethod ->>BeanPostProcess ->> BeanPostProcess PostConstruct ->>
+    //beanDefinition -->> 查找构造方法 反射 创建初始对象 --》注入到objectFactory中--》 放入三级缓存  objectFactory中  factoryBean  -->>调用factoryBean.get() 获取初始对象或代理对象 放入二级缓存中 --
+    // BeanFactory  InitializingBean  -->> BeanFactory initMethod ->>BeanPostProcess ->> BeanPostProcess PostConstruct ->>放入一级缓存中--》清除二三级缓存
     //BeanPostProcess PreDestroy -->> BeanFactory destroyMethod -->> BeanFactory DisposableBean -->> distory
     //容器初始化 加载完bean文件，bean未初始化 加载BeanFactoryPostProcessor 可获取配置类信息 包括设定是否单例模式
     //beanPostProcessor bean初始化之后  先调用before 再调用bean的init方法，再调用after方法
